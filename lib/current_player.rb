4c6b853a478dba6f482a814ec9e73b1a35c79e0b
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-09-21 16:43:54 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count % 2
+    "X"
+  else "O"
+  end
+end
+  
